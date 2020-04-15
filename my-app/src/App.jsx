fb9cc8e7d100b4fcefafb54b3c0f4ef38f4ca35b
Won Jung <wjung6799@outlook.com>
2020-04-15 00:27:36 -0700

    make it better

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Headerbar from './Components/Headerbar'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Headerbar />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ my-app/src/Components/Work.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Work extends React.Component {
+  render() {
+    return (
+      <div>
+        <div> FUCK YOUUUU </div>
+      </div>
+    )
+  }
+}
+
+export default Experience
